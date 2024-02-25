964940c6bf6a4fff5f7acddc2fda7a39c96b805b
evaristofm <evaristofm@gmail.com>
2024-02-25 19:19:04 -0300

    migrations

--- post/views.py
+++ post/views.py
@@ -13,24 +13,28 @@ class PostViewSet(ViewSet):
     queryset = Post.objects.all()
     permission_classes = [IsAuthenticated]
 
+    def get_object(self):
+        queryset = get_object_or_404(self.queryset, pk=self.kwargs.get('pk'))
+        return queryset
+
     def list(self, request):
         return Response(self.serializer_class(self.queryset, many=True).data, status=status.HTTP_200_OK)
 
     def create(self, request, *args, **kwargs):
         request.data['user'] = request.user.id
-        serializer = PostSerializer(data=request.data)
+        serializer = self.serializer_class(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
-    def retrieve(self, request, id=None):
-        post = get_object_or_404(self.queryset, pk=id)
+    def retrieve(self, request, pk=None):
+        post = get_object_or_404(self.queryset, pk=pk)
         serializer = self.serializer_class(post)
         return Response(serializer.data)
 
-    def remove(self, request):
-        post = Post.objects.get(id=self.kwargs.get('id'))
+    def delete(self, request, pk=None):
+        post = get_object_or_404(self.queryset, pk=pk)
         post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
